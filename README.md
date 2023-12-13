e51608bfe62be7d7477c7d2ae6712e2cc042781a
muhamadfarrelrizqullah <farrelswag27@gmail.com>
2023-12-13 13:00:04 +0700

    W13 P1

---
+++ README.md
@@ -0,0 +1,3 @@
+# stream_farrel
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/stream_farrel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stream_farrel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
